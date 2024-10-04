4a4231bb93cd4b67a1769f71a8355de780fb89a8
cbh778899 <cbh778899@outlook.com>
2024-10-04 16:19:57 +1000

    try-catch to avoid app crash
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- actions/azure-speech.js
+++ actions/azure-speech.js
@@ -13,18 +13,23 @@ import {default as sdk} from 'microsoft-cognitiveservices-speech-sdk'
  */
 export default function STT(buffer) {
     return new Promise(resolve=>{
-        const audioConfig =  sdk.AudioConfig.fromWavFileInput(buffer);
-        const speechConfig = sdk.SpeechConfig.fromSubscription(
-            process.env.AZURE_SPEECH_KEY, process.env.AZURE_SPEECH_REGION
-        );
-        speechConfig.speechRecognitionLanguage = 'en-us'
-    
-        const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-        recognizer.recognizeOnceAsync(result => {
-            const t = result.text
-            resolve({text: t})
-        }, err => {
-            resolve({error: err})
-        });
+        try {
+            const audioConfig = sdk.AudioConfig.fromWavFileInput(buffer);
+            const speechConfig = sdk.SpeechConfig.fromSubscription(
+                process.env.AZURE_SPEECH_KEY, process.env.AZURE_SPEECH_REGION
+            );
+            speechConfig.speechRecognitionLanguage = 'en-us'
+        
+            const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+            recognizer.recognizeOnceAsync(result => {
+                const t = result.text
+                resolve({text: t})
+            }, err => {
+                resolve({error: err})
+            });
+        } catch(error) {
+            console.error(error);
+            resolve({ error })
+        }
     })
 }
